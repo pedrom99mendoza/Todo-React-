69a15b4c7c14f9a84b1e07bf257f7b73d6931d37
pedro99mendoza <pedromenfer123@gmail.com>
2024-03-19 21:18:00 +0100

    first commit

---
+++ src/components/todo.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import "./todoApp.css";
+
+export default function Todo({ item , onUpdate, onDelete}) {
+  const [isEdit, setIsEdit] = useState(false);
+
+  
+
+  function FormEdit() {
+
+    const [newValue, setNewValue] = useState(item.title);
+    function handleSubmit(e){
+      e.preventDefault();
+    }
+
+    function handleChange(e){
+      const value = e.target.value;
+      setNewValue(value);
+      
+    }
+
+    function handleClickUpdateTodo(){
+        onUpdate(item.id, newValue);
+        setIsEdit(false);
+    }
+    return (
+      <form className='todoUpdateForm' onSubmit={handleSubmit}>
+        <input type='text' className='todoInput' onChange={handleChange} value={newValue}/>
+        <button className='button' onClick={handleClickUpdateTodo}>Update</button>
+      </form> 
+    );
+  }
+
+  function TodoElement() {
+    return (
+      <div className='todoInfo'>
+        {item.title} <button className='button' onClick={() => setIsEdit(true)}>Edit</button>
+        <button className='button' onClick={(e)=>onDelete(item.id)}>Delete</button>
+      </div>
+    );
+  }
+
+  return (
+    <div className='todo'>
+      {isEdit ? <FormEdit /> : <TodoElement />}
+    </div>
+  );
+}
